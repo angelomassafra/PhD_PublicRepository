83a8b540dbc703c24b8639b46968d1471f82c191
Angelo Massafra <115364259+angelomassafra@users.noreply.github.com>
2024-01-17 17:07:43 +0100

    Add files via upload

---
+++ TopologicalBIM/EnergyPlus/BIMtoBEM_SampleBuilding.bat
@@ -0,0 +1,3 @@
+C:
+cd\BIMtoBEM\BIMtoBEM_SampleBuilding\EnergyPlus\
+"C:/openstudio-3.0.0/EnergyPlus/\Epl-run" C:\BIMtoBEM\BIMtoBEM_SampleBuilding\EnergyPlus\BIMtoBEM_SampleBuilding C:\BIMtoBEM\BIMtoBEM_SampleBuilding\EnergyPlus\BIMtoBEM_SampleBuilding idf c:\ladybug\ITA_Bologna-Borgo.Panigale.161400_IGDG\ITA_Bologna-Borgo.Panigale.161400_IGDG.epw EP N nolimit N N 0 Y
